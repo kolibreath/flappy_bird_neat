ffc10a1505d09f8e8dede77eeb29a49af4b513f9
kolibreath <734780178@qq.com>
2020-10-30 13:58:11 +0800

    [ADD] base frame

--- code/bird.py
+++ code/bird.py
@@ -1,6 +1,18 @@
 import pygame
-class Bird:
+import torch
+import numpy as np
+from torch import nn
+from torch.nn import init
 
+
+class FlattenLayer(torch.nn.Module):
+    def __init__(self):
+        super(FlattenLayer, self).__init__()
+
+    def forward(self, x):  # x shape: (batch, *, *, ...)
+        return x.view(x.shape[0], -1)
+
+class Bird:
     
     def __init__(self, game):
         self.bird_downflap = pygame.transform.scale2x(pygame.image.load('./assets/bluebird-downflap.png').convert_alpha())
@@ -15,9 +27,26 @@ class Bird:
         self.width = 0  
         self.height = 0
         
+        self.distance = 0
+        
+        self.num_inputs, self.num_outputs = 6, 1
+        self.num_hiddens = 10
+        
+        self.net = nn.Sequential({
+            FlattenLayer(),
+            nn.Linear(self.num_inputs, self.num_hiddens),
+            nn.ReLU(),
+            nn.Linear(self.num_hiddens, self.outputs)
+        })
+        
+        for param in net.parameters():
+            init.normal_(param, mean=0, std=0.01)
+        
+        self.optimizer = torch.optim.SGD(net.parameters(), lr = 0.5)
+        self.loss = torch.nn.CrossEntropyLoss()
     
+        
     # member functions
-
     def rotate_bird(self, bird):
         new_bird = pygame.transform.rotozoom(bird, - self.bird_movement * 3, 1)
         return new_bird
@@ -28,4 +57,19 @@ class Bird:
         new_bird_rect = new_surface.get_rect(center=(100, self.bird_rect.centery))
         return new_surface, new_bird_rect
 
-    
+    # todo 
+    # get six input params
+    def get_inputs(self):
+        width_1 = 0
+        height_1= 0 
+        height_2= 0 
+        width_2 = 0
+        height_3= 0
+        height_4= 0
+        
+        return width_1, height_1, height_2, width_2, height_3, height_4
+
+    # todo 
+    # fitness function
+    def distance(self):
+        return self.distance
